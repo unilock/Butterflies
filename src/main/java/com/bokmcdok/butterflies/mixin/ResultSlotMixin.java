43116299e053ba4c888a9bd6469a5455703f1d2e
unilock <unilock@fennet.rentals>
2023-11-01 08:06:10 -0400

    Fix bottled butterfly crafting
    Thanks Porting Lib!
    https://github.com/Fabricators-of-Create/Porting-Lib/blob/4819c4c23c26b18256eaa41413e967b7880e0624/base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/ResultSlotMixin.java
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/com/bokmcdok/butterflies/mixin/ResultSlotMixin.java
+++ src/main/java/com/bokmcdok/butterflies/mixin/ResultSlotMixin.java
@@ -4,7 +4,6 @@ import com.bokmcdok.butterflies.registries.ItemRegistry;
 import com.bokmcdok.butterflies.world.CompoundTagId;
 import com.bokmcdok.butterflies.world.item.BottledButterflyItem;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.inventory.ResultSlot;
 import net.minecraft.world.item.ItemStack;
@@ -23,16 +22,16 @@ public class ResultSlotMixin {
 	@Final
 	private CraftingContainer craftSlots;
 
-	@Inject(method = "onTake", at = @At("HEAD"))
-	private void onTake(Player player, ItemStack craftingItem, CallbackInfo ci) {
-		if (!player.getCommandSenderWorld().isClientSide() && this.craftSlots.getItems().stream().map(ItemStack::getItem).toList().contains(ItemRegistry.BOTTLED_BUTTERFLY)) {
-			this.craftSlots.getItems().forEach(recipeItem -> {
+	@Inject(method = "checkTakeAchievements", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;onCraftedBy(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;I)V", shift = At.Shift.AFTER))
+	private void onTake(ItemStack craftingItem, CallbackInfo ci) {
+		if (Objects.equals(craftingItem.getItem(), ItemRegistry.BOTTLED_BUTTERFLY)) {
+			for (ItemStack recipeItem : craftSlots.getItems()) {
 				if (Objects.equals(recipeItem.getItem(), ItemRegistry.BUTTERFLY_NET)) {
 					CompoundTag tag = recipeItem.copy().getOrCreateTag();
 					String entityId = tag.getString(CompoundTagId.ENTITY_ID);
 					BottledButterflyItem.setButterfly(craftingItem, entityId);
 				}
-			});
+			}
 		}
 	}
 }
